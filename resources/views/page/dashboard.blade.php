9fec4bfec6387160dac0995f481a07425fde94a7
adittyas <aditya.sandy23@gmail.com>
2020-01-05 16:15:26 +0700

    update

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -6,7 +6,9 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Booking Activity</div>
+                <div class="card-header">Activity @php
+                    echo date('Y')
+                    @endphp</div>
 
                 <div class="card-body">
                     @if (session('status'))
